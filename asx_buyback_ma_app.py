c4e6c1cf1be488574487fa0ec9c1028c20207bf0
Bouddha0357 <kevin_sparks@orange.fr>
2025-09-29 17:06:57 +1000

    Update asx_buyback_ma_app.py
    Removed DRO

--- asx_buyback_ma_app.py
+++ asx_buyback_ma_app.py
@@ -11,7 +11,7 @@ st.title("📈 ASX Stocks to Potentially Buyback Data Downloader - Closing Price
 # -----------------------------
 # ASX Ticker List (from your screenshot)
 tickers = [
-    "LYC",  "IMM",  "WHK",  "A2M",  "SOL",  "DRO",  "LGP",  "PLY",  "RFG",  "BBOZ" 
+    "LYC",  "IMM",  "WHK",  "A2M",  "SOL",  "LGP",  "PLY",  "RFG",  "BBOZ" 
 ]
 
 # Add ".AX" to each ticker for Yahoo Finance
@@ -54,5 +54,6 @@ if all_data:
     )
 else:
     st.error("No data was fetched. Please try again later or check ticker symbols.")
+
 
 
